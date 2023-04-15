6bc33e7ed4beca753bf1859762df0c6d1728baa5
Anastasiya Klevko <klevko.anastasiya@gmail.com>
2023-04-15 22:46:11 +0300

    feat: add recursive-depth solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,17 +13,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(...members) {
-  let membersArr = Array.from(arguments);
-  let notSorted = membersArr.reduce(function(acc, item) {
-    if( typeof item === 'string') {
-      let part = item.toUpperCase.slice(0, 1);
-      return acc.push(part);
-    }
-  },[]);
-  if(...notSorted !== '') {
-    return notSorted.sort();
-  }
+function createDreamTeam(/*members*/) {
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
 }
 
 module.exports = {
