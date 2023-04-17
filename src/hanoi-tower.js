fba7855e12bad6117faa29d1970e2f8c39e24361
Anastasiya Klevko <klevko.anastasiya@gmail.com>
2023-04-18 00:45:35 +0300

    feat: add hanoi solution

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -17,7 +17,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 function calculateHanoi(disksNumber, turnsSpeed) {
   let result = {};
   result.turns = 2**disksNumber - 1;
-  result.seconds = Math.round((result.turns * 3600)/turnsSpeed);
+  result.seconds = Math.floor((result.turns * 3600)/turnsSpeed);
   return result;
 }
 
